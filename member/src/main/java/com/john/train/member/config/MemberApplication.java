5ba887dfc86b5e5f91e117f112c829d4949ffb0b
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-03-23 09:55:57 +0800

    修改启动日志，增加/member，修改logback日志

--- member/src/main/java/com/john/train/member/config/MemberApplication.java
+++ member/src/main/java/com/john/train/member/config/MemberApplication.java
@@ -1,8 +1,11 @@
 package com.john.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * @author johnconstantine
@@ -10,9 +13,13 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.john")
 public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}/hello", env.getProperty("server.port"));
     }
 
 }
