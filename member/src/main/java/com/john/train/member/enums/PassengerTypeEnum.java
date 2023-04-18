87113715af27bd2389189a268985a6454ec72bf2
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-04-18 23:35:07 +0800

    增加乘车人类型枚举

---
+++ member/src/main/java/com/john/train/member/enums/PassengerTypeEnum.java
@@ -0,0 +1,55 @@
+package com.john.train.member.enums;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author johnconstantine
+ */
+
+public enum PassengerTypeEnum {
+    /**
+     * 成人\儿童\学生
+     */
+    ADULT("1", "成人"),
+    CHILD("2", "儿童"),
+    STUDENT("3", "学生");
+
+    private String code;
+
+    private String desc;
+
+    PassengerTypeEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public static List<HashMap<String, String>> getEnumList() {
+        List<HashMap<String, String>> list = new ArrayList<>();
+        for (PassengerTypeEnum anEnum : EnumSet.allOf(PassengerTypeEnum.class)) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("code", anEnum.code);
+            map.put("desc", anEnum.desc);
+            list.add(map);
+        }
+        return list;
+    }
+}
