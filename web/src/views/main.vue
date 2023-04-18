204892fc88cafe1f369e3f8b89477d7998cb18df
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-04-18 21:52:48 +0800

    为axios增加登录相关拦截：请求时带上token，返回时校验返回码是不是401

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -12,16 +12,18 @@
                 <a-layout-content
                         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
                 >
-                    Content
+                    所有会员数量{{ count }}
                 </a-layout-content>
             </a-layout>
         </a-layout>
     </a-layout>
 </template>
 <script>
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 import TheHeaderView from "@/components/the-header.vue";
 import TheSiderView from "@/components/the-sider.vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
 
 export default defineComponent({
     components: {
@@ -30,7 +32,19 @@ export default defineComponent({
 
     },
     setup() {
-        return {};
+        const count = ref(0)
+        axios.get("/member/member/count").then((response) => {
+            let data = response.data;
+            if (data.success) {
+                count.value = data.content;
+            } else {
+                notification.error({description: data.message});
+            }
+        });
+
+        return {
+            count
+        };
     },
 });
 </script>
