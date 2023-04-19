347af8ea644ef6f0baf6bff98f52d28a62937ed8
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-04-19 21:55:41 +0800

    使用线程本地变量存储会员信息

--- member/src/main/java/com/john/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/john/train/member/req/PassengerSaveReq.java
@@ -9,7 +9,6 @@ import java.util.Date;
  */
 public class PassengerSaveReq {
     private Long id;
-    @NotNull(message = "【会员ID】不能为空")
     private Long memberId;
     @NotNull(message = "【名字】不能为空")
     private String name;
--- member/src/main/java/com/john/train/member/service/PassengerService.java
+++ member/src/main/java/com/john/train/member/service/PassengerService.java
@@ -2,6 +2,7 @@ package com.john.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import com.john.train.common.context.LoginMemberContext;
 import com.john.train.common.util.SnowUtil;
 import com.john.train.member.domain.Passenger;
 import com.john.train.member.mapper.PassengerMapper;
@@ -22,6 +23,7 @@ public class PassengerService {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
         passenger.setId(SnowUtil.getSnowflakeNextId());
+        passenger.setMemberId(LoginMemberContext.getId());
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
