06e3e08939298f18dba54a4d63cc3c7251b51870
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-04-20 21:39:33 +0800

    乘车人编辑界面开发，解决没保存也会同步修改列表的问题

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -81,10 +81,11 @@ export default defineComponent({
         }];
 
         const onAdd = () => {
+            passenger.value = {};
             visible.value = true;
         };
         const onEdit = (record) => {
-            passenger.value = record;
+            passenger.value = window.Tool.copy(record);
             visible.value = true;
         };
 
