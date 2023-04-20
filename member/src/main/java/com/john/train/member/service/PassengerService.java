d2cb1a3bb0d0857cfa504996ca390611f010866f
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-04-20 21:17:25 +0800

    乘车人编辑接口开发

--- member/src/main/java/com/john/train/member/service/PassengerService.java
+++ member/src/main/java/com/john/train/member/service/PassengerService.java
@@ -34,11 +34,16 @@ public class PassengerService {
     public void save(PassengerSaveReq req) {
         DateTime now = DateTime.now();
         Passenger passenger = BeanUtil.copyProperties(req, Passenger.class);
-        passenger.setId(SnowUtil.getSnowflakeNextId());
-        passenger.setMemberId(LoginMemberContext.getId());
-        passenger.setCreateTime(now);
-        passenger.setUpdateTime(now);
-        passengerMapper.insert(passenger);
+        if (ObjectUtil.isNull(passenger.getMemberId())) {
+            passenger.setMemberId(LoginMemberContext.getId());
+            passenger.setId(SnowUtil.getSnowflakeNextId());
+            passenger.setCreateTime(now);
+            passenger.setUpdateTime(now);
+            passengerMapper.insert(passenger);
+        } else {
+            passenger.setUpdateTime(now);
+            passengerMapper.updateByPrimaryKey(passenger);
+        }
     }
 
     public PageResp<PassengerQueryResp> queryResp(PassengerQueryReq req) {
