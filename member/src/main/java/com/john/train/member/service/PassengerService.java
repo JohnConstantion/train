02f4ad167a3ee8e1ccac5b71c3e96744836a529c
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-04-20 16:45:54 +0800

    集成PageHelper，实现sql分页功能

--- member/src/main/java/com/john/train/member/service/PassengerService.java
+++ member/src/main/java/com/john/train/member/service/PassengerService.java
@@ -3,6 +3,7 @@ package com.john.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
 import com.john.train.common.context.LoginMemberContext;
 import com.john.train.common.util.SnowUtil;
 import com.john.train.member.domain.Passenger;
@@ -41,6 +42,7 @@ public class PassengerService {
         if (ObjectUtil.isNotNull(req.getMemberId())) {
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+        PageHelper.startPage(2, 2);
         List<Passenger> passengerList = passengerMapper.selectByExample(example);
         return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
     }
