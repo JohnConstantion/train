b7e949e89ae2f14a498e428ddc71fc3e6aa0847c
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-04-25 21:19:22 +0800

    制作queryResp.ftl模板

--- member/src/main/java/com/john/train/member/req/PassengerQueryReq.java
+++ member/src/main/java/com/john/train/member/req/PassengerQueryReq.java
@@ -6,21 +6,10 @@ import com.john.train.common.req.PageReq;
  * @author johnconstantine
  */
 public class PassengerQueryReq extends PageReq {
-    private Long memberId;
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
 
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("PassengerQueryReq{");
-        sb.append("memberId=").append(memberId);
-        sb.append('}');
-        return sb.toString();
+        return "PassengerQueryReq{" +
+                "} " + super.toString();
     }
 }
