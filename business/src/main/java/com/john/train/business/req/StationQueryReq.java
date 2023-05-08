a8b247b139bdb7f15158203ce535a83502898539
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-05-08 16:07:44 +0800

    增加车站表，生成持久层代码及前后端代码

---
+++ business/src/main/java/com/john/train/business/req/StationQueryReq.java
@@ -0,0 +1,12 @@
+package com.john.train.business.req;
+
+import com.john.train.common.req.PageReq;
+
+public class StationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "StationQueryReq{" +
+                "} " + super.toString();
+    }
+}
