2c4aff80d0af01616b91b9ecf447bcb8454490ae
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-05-10 02:53:46 +0800

    禁止任务并发执行

--- batch/src/main/java/com/john/train/batch/job/TestJob.java
+++ batch/src/main/java/com/john/train/batch/job/TestJob.java
@@ -1,16 +1,23 @@
 package com.john.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
 /**
  * @author JohnConstantine
  * @date 2023/5/10 02:46
  */
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("TestJob TEST");
+    public void execute(JobExecutionContext context) {
+        System.out.println("TestJob TEST开始");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("TestJob TEST结束");
     }
 }
