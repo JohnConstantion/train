59fd445cf4ae19ed421d58cd2d6cd4ce8b76e077
chenxianjun_ext <chenxianjun_ext@cop.cmsoft.com.cn>
2023-05-09 16:29:21 +0800

    为火车座位页面增加车次查询条件

--- business/src/main/java/com/john/train/business/req/TrainSeatQueryReq.java
+++ business/src/main/java/com/john/train/business/req/TrainSeatQueryReq.java
@@ -4,6 +4,16 @@ import com.john.train.common.req.PageReq;
 
 public class TrainSeatQueryReq extends PageReq {
 
+    private String trainCode;
+
+    public String getTrainCode() {
+        return trainCode;
+    }
+
+    public void setTrainCode(String trainCode) {
+        this.trainCode = trainCode;
+    }
+
     @Override
     public String toString() {
         return "TrainSeatQueryReq{" +
