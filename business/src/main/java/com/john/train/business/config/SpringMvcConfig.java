af81abeed8c2b1ef6ba99bda6b4bf750257f28f7
JohnConstantine <constantinechen2@gmail.com>
2023-05-13 22:19:26 +0800

    为business增加日志拦截器

---
+++ business/src/main/java/com/john/train/business/config/SpringMvcConfig.java
@@ -0,0 +1,22 @@
+package com.john.train.business.config;
+
+import com.john.train.common.interceptor.LogInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author johnconstantine
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor).addPathPatterns("/**");
+    }
+}
