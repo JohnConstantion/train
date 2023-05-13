97557f95720418696252bbf380a2b839a0e0dd33
JohnConstantine <constantinechen2@gmail.com>
2023-05-13 21:51:10 +0800

    增加每日座位表，生成前后端代码

---
+++ business/src/main/java/com/john/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.john.train.business.req;
+
+import com.john.train.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
